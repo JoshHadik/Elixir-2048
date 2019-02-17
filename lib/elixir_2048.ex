9bb0d81f79f13e01416e44e25834401debcf467e
Josh Hadik <josh.hadik@gmail.com>
2019-02-16 19:58:40 -0600

    refactor to join MoveRight and MoveLeft logic into Game module

--- lib/elixir_2048.ex
+++ lib/elixir_2048.ex
@@ -1,22 +1,13 @@
 defmodule Elixir2048 do
   alias Elixir2048.Game, as: Game
-  alias Elixir2048.MoveRight, as: MoveRight
-  alias Elixir2048.MoveLeft, as: MoveLeft
 
   def move_right(list) do
-    list
-    |> new_game()
-    |> MoveRight.call()
-    |> return_list()
+    list |> Game.new() |> Game.move_right() |> return_list()
   end
 
   def move_left(list) do
-    list
-    |> new_game()
-    |> MoveLeft.call()
-    |> return_list()
+    list |> Game.new() |> Game.move_left() |> return_list()
   end
 
-  defp new_game(list), do: %Game{list: list}
   defp return_list(%Game{list: list}), do: list
 end
--- lib/elixir_2048/game.ex
+++ lib/elixir_2048/game.ex
@@ -1,3 +1,82 @@
 defmodule Elixir2048.Game do
-  defstruct list: [], values: [], spaces: 0
+  alias Elixir2048.Game, as: Game
+  defstruct list: [], values: [], spaces: []
+
+  # New game
+
+  def new(list), do: %Game{list: list}
+
+  # Move by direction
+
+  def move_right(game) do
+    game
+    |> split_values_and_spaces()
+    |> combine_matched_values(:right_to_left)
+    |> add_spaces_to_list(:left)
+  end
+
+  def move_left(game) do
+    game
+    |> split_values_and_spaces()
+    |> combine_matched_values(:left_to_right)
+    |> add_spaces_to_list(:right)
+  end
+
+  # Splits Values and Spaces
+
+  def split_values_and_spaces(game = %Game{list: list}) do
+    %Game{ List.foldr(list, game, &add_space_or_value/2) | list: []}
+  end
+
+  defp add_space_or_value(space, game = %Game{spaces: spaces})
+  when is_nil(space) do
+    %Game{ game | spaces: [space | spaces]}
+  end
+
+  defp add_space_or_value(value, game = %Game{values: values}) do
+    %Game{ game | values: [value | values] }
+  end
+
+  # Add Combined Values to List
+
+  defp combine_matched_values(game = %Game{values: values}, :left_to_right) do
+    values
+    |> combine_pairs()
+    |> add_new_values_to_list(game)
+  end
+
+  defp combine_matched_values(game = %Game{values: values}, :right_to_left) do
+    values
+    |> Enum.reverse()
+    |> combine_pairs()
+    |> Enum.reverse()
+    |> add_new_values_to_list(game)
+  end
+
+  defp combine_pairs([]), do: []
+  defp combine_pairs([v1, v2 | tail]) when v1 == v2 do
+    [ v1 + v2 | combine_pairs(tail) ]
+  end
+
+  defp combine_pairs([v1 | tail]) do
+    [ v1 | combine_pairs(tail) ]
+  end
+
+  defp add_new_values_to_list(new_values, game) do
+    spaces = List.duplicate(nil, (length(game.values) - length(new_values)))
+    %Game{ game |
+      list: new_values,
+      spaces: game.spaces ++ spaces,
+      values: [] }
+  end
+
+  # Add Spaces to List
+
+  defp add_spaces_to_list(game = %Game{list: list, spaces: spaces}, :right) do
+    %Game{ game | list: list ++ spaces, spaces: [] }
+  end
+
+  defp add_spaces_to_list(game = %Game{list: list, spaces: spaces}, :left) do
+    %Game{ game | list: spaces ++ list, spaces: [] }
+  end
 end
